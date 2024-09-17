b637f932885a427f8c356099ea2e52b57ec83198
Sudip Khatri <sudipkhatri036@gmail.com>
2024-09-18 01:45:00 +0545

    Added Lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,29 @@
-import { useEffect, useState } from "react";
+import { lazy, Suspense, useEffect, useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
-import Home from "./pages/Home";
-import Chat from "./pages/Chat";
-import Speech from "./pages/Speech";
-import Marketing from "./pages/Marketing";
-import Seminar from "./pages/Seminar";
-import News from "./pages/News";
-import CompanyGuide from "./pages/CompanyGuide";
-import Inquiry from "./pages/Inquiry";
-import PostPage from "./pages/PostPage";
-import PostPageContent from "./components/postPageContent/PostPageContent";
-import PageNotFound from "./pages/PageNotFound";
+import Loader from "./components/loader/Loader";
+// import Home from "./pages/Home";
+// import Chat from "./pages/Chat";
+// import Speech from "./pages/Speech";
+// import Marketing from "./pages/Marketing";
+// import Seminar from "./pages/Seminar";
+// import News from "./pages/News";
+// import CompanyGuide from "./pages/CompanyGuide";
+// import Inquiry from "./pages/Inquiry";
+// import PostPage from "./pages/PostPage";
+// import PageNotFound from "./pages/PageNotFound";
+
+const Home = lazy(() => import("./pages/Home"));
+const Chat = lazy(() => import("./pages/Chat"));
+const Speech = lazy(() => import("./pages/Speech"));
+const Marketing = lazy(() => import("./pages/Marketing"));
+const Seminar = lazy(() => import("./pages/Seminar"));
+const News = lazy(() => import("./pages/News"));
+const CompanyGuide = lazy(() => import("./pages/CompanyGuide"));
+const Inquiry = lazy(() => import("./pages/Inquiry"));
+const PostPage = lazy(() => import("./pages/PostPage"));
+const PageNotFound = lazy(() => import("./pages/PageNotFound"));
 
 function App() {
   const [isHome, setIsHome] = useState(true);
@@ -24,18 +35,20 @@ function App() {
   return (
     <div className="app">
       <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/chat" element={<Chat />} />
-        <Route path="/speech" element={<Speech />} />
-        <Route path="/marketing" element={<Marketing />} />
-        <Route path="/seminar" element={<Seminar />} />
-        <Route path="/news" element={<News />} />
-        <Route path="/newspost" element={<PostPage />} />
-        <Route path="/companyguide" element={<CompanyGuide />} />
-        <Route path="/inquiry" element={<Inquiry />} />
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/chat" element={<Chat />} />
+          <Route path="/speech" element={<Speech />} />
+          <Route path="/marketing" element={<Marketing />} />
+          <Route path="/seminar" element={<Seminar />} />
+          <Route path="/news" element={<News />} />
+          <Route path="/newspost" element={<PostPage />} />
+          <Route path="/companyguide" element={<CompanyGuide />} />
+          <Route path="/inquiry" element={<Inquiry />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </Suspense>
       <Footer />
     </div>
   );
---
+++ src/components/loader/Loader.jsx
@@ -0,0 +1,17 @@
+import "./Loader.css";
+
+function Loader() {
+  return (
+    <div className="loader">
+      <div className="loadingspinner">
+        <div className="square1"></div>
+        <div className="square2"></div>
+        <div className="square3"></div>
+        <div className="square4"></div>
+        <div className="square5"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Loader;
